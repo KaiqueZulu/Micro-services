2e143d319e6431ce4f8fee4045492813fd7c5d92
Kaique Pires <kaiqueroberto.pires@gmail.com>
2021-11-16 20:47:25 -0300

    Finalizando o modulo do AUTH-API

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,17 +1,24 @@
 import express from "express";
+import UserRoutes from "./src/modules/user/routes/UserRoutes.js";
+import * as db from "./src/config/db/initialData.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
-app.get('/api/status',(req,res)=>{
+db.createInitialData();
+
+app.get('/api/status', (req, res) => {
     return res.status(200).json({
         service: "Auth-API",
         status: "up",
         httpStatus: 200,
-    })
-})
+    });
+});
 
-app.listen(PORT, ()=>{
+app.use(express.json());
+app.use(UserRoutes);
+
+app.listen(PORT, () => {
     console.info(`Server started successfuly at port ${PORT}`)
-})
+});
